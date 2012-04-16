eec7a1b863e33bcde29f49f4261f57f57a16fb77
Mr. Nobody <tobiaskoenig1@googlemail.com>
2012-04-16 15:54:29 +0200

    testdata

--- src/mygame/Main.java
+++ src/mygame/Main.java
@@ -7,6 +7,7 @@ import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.shape.Box;
+import com.jme3.scene.Node;
 
 /**
  * test
@@ -21,14 +22,8 @@ public class Main extends SimpleApplication {
 
     @Override
     public void simpleInitApp() {
-        Box b = new Box(Vector3f.ZERO, 1, 1, 1);
-        Geometry geom = new Geometry("Box", b);
-
-        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        mat.setColor("Color", ColorRGBA.Blue);
-        geom.setMaterial(mat);
-
-        rootNode.attachChild(geom);
+        createGround(10, 1);
+        //achChild(geom);
     }
 
     @Override
@@ -39,5 +34,21 @@ public class Main extends SimpleApplication {
     @Override
     public void simpleRender(RenderManager rm) {
         //TODO: add render code
+    }
+    
+    private void createGround(int count,int blocksize){
+        Node ground = new Node("ground");
+        for(int x=0;x<count*blocksize*2;x+=blocksize*2){
+            for(int z=0;z<count*blocksize*2;z+=blocksize*2){
+                Box b = new Box(Vector3f.ZERO, blocksize, blocksize, blocksize);
+                Geometry geom = new Geometry("Box"+x+z, b);
+                Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+                mat.setColor("Color", ColorRGBA.randomColor());
+                geom.setMaterial(mat);
+                geom.setLocalTranslation( new Vector3f(x+(x*0.1f), -10, z+(z*0.1f)));
+                ground.attachChild(geom);
+            }
+        }
+        rootNode.attachChild(ground);
     }
 }
