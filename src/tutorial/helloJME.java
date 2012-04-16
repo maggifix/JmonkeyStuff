bb3388213676772fa7da9c16045c0d2cadb94a68
Mr. Nobody <tobiaskoenig1@googlemail.com>
2012-04-16 14:32:30 +0200

    first push

---
+++ src/tutorial/helloJME.java
@@ -0,0 +1,30 @@
+package tutorial;
+
+import com.jme3.app.SimpleApplication;
+import com.jme3.material.Material;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.shape.Box;
+import com.jme3.math.ColorRGBA;
+ 
+/** Sample 1 - how to get started with the most simple JME 3 application.
+ * Display a blue 3D cube and view from all sides by
+ * moving the mouse and pressing the WASD keys. */
+public class helloJME extends SimpleApplication{
+ 
+    public static void main(String[] args){
+        helloJME app = new helloJME();
+        app.start(); // start the game
+    }
+ 
+    @Override
+    public void simpleInitApp() {
+        Box b = new Box(Vector3f.ZERO, 1, 1, 1); // create cube shape at the origin
+        Geometry geom = new Geometry("Box", b);  // create cube geometry from the shape
+        Material mat = new Material(assetManager,
+          "Common/MatDefs/Misc/Unshaded.j3md");  // create a simple material
+        mat.setColor("Color", ColorRGBA.Green);   // set color of material to blue
+        geom.setMaterial(mat);                   // set the cube's material
+        rootNode.attachChild(geom);              // make the cube appear in the scene
+    }
+}
